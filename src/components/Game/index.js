59bd0231846e62f65a9ca8d6113be74d3a552446
Victor Lundberg <victorlundberg0@gmail.com>
2021-08-19 10:24:25 +0200

    first commit

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import './style.css';
 
 function Game() {
-  let counter = 7;
+  const [lives, setLives] = React.useState(7);
     return (
       <div id="game">
         <h3 id="mini-header" className="titles">Guess the word:</h3>
         <h1 id="word" className="titles">_ _ _ _ _</h1>
+        <p>{lives}</p>
       </div>
     );
   }
